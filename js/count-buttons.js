926a14e1cae38ac425946b8c51fed6ac5784e4ac
Юлия Кольцова <yuliykoo@mail.ru>
2023-09-05 15:08:39 +0400

    Добавляет ф. изменения количества товара в итого

--- js/count-buttons.js
+++ js/count-buttons.js
@@ -10,12 +10,44 @@ const PriceGoods = {
 
 // Цены на единицу товара без скидки
 const OldPriceGoods = {
-  1: 522,
-  2: 10500.235,
-  3: 247,
+  1: 1051,
+  2: 11500.235,
+  3: 475,
 }
 
-// Изменение стоимости товаров в карточках товаров
+// Функция отображения сумм в блоке Итого
+const calculatedTotal = (array, element) => {
+  // Добавляем в массив все цены
+  const arrayPrices = new Array(); 
+  for (let i = 0; i < array.length; i++) {
+    arrayPrices.push(Number(array[i].textContent.replace(/[^0-9]/g,"")));
+  }
+  // Забираем каждый второй элемент массива, т.к. в разметке дублируется цена для mobile и desktop/tablet
+  const sortedArrayPrices = arrayPrices.filter((item, index) => index % 2);
+  // Суммируем
+  const sumOfNumbers = sortedArrayPrices.reduce((acc, number) => acc + number, 0);
+  element.textContent = `${(sumOfNumbers).toLocaleString()} com`;
+}
+
+// Функция отображения сумм в блоке Итого
+const calculatedTotalCount = (array, element) => {
+  // Добавляем в массив все цены
+  const arrayPrices = new Array(); 
+  for (let i = 0; i < array.length; i++) {
+    arrayPrices.push(Number(array[i].value.replace(/[^0-9]/g,"")));
+  }
+  // Забираем каждый второй элемент массива, т.к. в разметке дублируется цена для mobile и desktop/tablet
+  const sortedArrayPrices = arrayPrices.filter((item, index) => index % 2);
+  // Суммируем
+  const sumOfNumbers = sortedArrayPrices.reduce((acc, number) => acc + number, 0);
+  if (sumOfNumbers > 1) {
+    element.textContent = `${(sumOfNumbers).toLocaleString()} товара`;
+  } else {
+    element.textContent = `${(sumOfNumbers).toLocaleString()} товар`;
+  }
+}
+
+// Изменение стоимости товаров в карточках товаров и в полях Итого
 const calculationCardPrices = (evt) => {
   // Количество товаров
   const count = Number(evt.target.parentElement.querySelector("input").value);
@@ -32,20 +64,24 @@ const calculationCardPrices = (evt) => {
   oldPrice.forEach(element => {
     element.textContent = `${(Math.round(count * oldPriceOnetem)).toLocaleString()} com`;
   });
-  // Находим все элементы с суммой в карточках товаров
-  const all = document.querySelectorAll(".goods-card__info-price");
-  // Добавляем в массив все цены
-  const arrayPrices = new Array(); 
-  for (let i = 0; i < all.length; i++) {
-    arrayPrices.push(Number(all[i].textContent.replace(/[^0-9]/g,"")));
-  }
-  // Забираем каждый второй элемент массива, т.к. в разметке дублируется цена для mobile и desktop/tablet
-  const sortedArrayPrices = arrayPrices.filter((item, index) => index % 2);
-  // Суммируем
-  const sumOfNumbers = sortedArrayPrices.reduce((acc, number) => acc + number, 0);
-  const total = document.querySelector(".total__total")
-  total.textContent = `${(sumOfNumbers).toLocaleString()} com`;
-  console.log(sumOfNumbers)
+
+  // Сумма со скидкой
+  const prices = document.querySelectorAll(".goods-card__info-price");
+  const total = document.querySelector(".total__total");
+  calculatedTotal(prices, total);
+  // Сумма без скидки
+  const priceWithoutSale = document.querySelectorAll(".goods-card__info-old-price");
+  const totalWithoutSale = document.querySelector(".total__сell--without-discount");
+  calculatedTotal(priceWithoutSale, totalWithoutSale)
+
+  // Расчет и отображение скидки
+  const totalDiscount = document.querySelector(".total__сell-discoun");
+  totalDiscount.textContent = `${((Number(total.textContent.replace(/[^0-9]/g,""))) - Number(totalWithoutSale.textContent.replace(/[^0-9]/g,""))).toLocaleString()} com`
+
+  // Расчет и отображение количества товара
+  const totalCount = document.querySelector(".total__сell--count");
+  const allCount = document.querySelectorAll(".count__input");
+ calculatedTotalCount(allCount, totalCount);
 }
 
 // Добавляем слушатель(клик) на список карточек товаров в корзине (те, что есть в наличии) и реализуем работу кнопок + и - (значение в инпуте >= 1)
